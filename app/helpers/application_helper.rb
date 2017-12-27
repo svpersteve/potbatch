3ea8b04d9044faa48d974bd08a6c58061b1397b1
Steve Brewer <steve.brewer@futurelearn.com>
2017-12-27 15:10:10 +0000

    Referral link helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def set_page_description(description)
     @page_description = description
   end
+
+  def instant_pot_link
+    'http://amzn.to/2BZL1lL'
+  end
+
+  def tub_link
+    'http://amzn.to/2BXUFoW'
+  end
 end
