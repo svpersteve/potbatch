ac3cc858c517d2925b7e5776ae779faa708155e9
Steve Brewer <steve.brewer@futurelearn.com>
2017-12-27 15:09:45 +0000

    Page title and description helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def set_page_title(title)
+    @page_title = title + ' | Pot Batch'
+  end
+
+  def set_page_description(description)
+    @page_description = description
+  end
 end
